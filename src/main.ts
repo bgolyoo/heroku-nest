bd4d4473cf37d08ee56666c792c5c83012e52335
Bodor Gergely <bgolyoo@gmail.com>
2021-02-23 21:28:08 +0100

    chore: create project

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000, () => Logger.log(`Server is listening on port ${process.env.PORT || 3000}...`));
+}
+
+bootstrap();
